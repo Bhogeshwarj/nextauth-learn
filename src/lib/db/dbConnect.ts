27e79d98352eaec5e4b5c0600008b56596939159
Bhogeshwar Jadhav <bhogeshwarj@gmail.com>
2024-05-23 13:14:23 +0530

    Added the helper and routes for sending emails on signup

--- src/lib/db/dbConnect.ts
+++ src/lib/db/dbConnect.ts
@@ -1,1 +1,28 @@
 import mongoose from "mongoose";
+
+type ConnectionObject = {
+    isConnected ?: number
+}
+
+const connection: ConnectionObject = {};
+
+async function dbConnect(): Promise<void>{
+    if(connection.isConnected) {
+        console.log("Using existing connection");
+        return ;
+    }
+
+    try{
+    
+    const db = await mongoose.connect(process.env.DATABASE_URL || '', {})
+    connection.isConnected = db.connections[0].
+    readyState
+    console.log("DB connected Successfully");
+
+    } catch(error) {
+        console.log("Connection failed ",error);
+        process.exit(  1)
+    }
+}
+
+export default dbConnect;
